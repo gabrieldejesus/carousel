c44207e970c84f139bf70f3f0076c536643b1c40
Gabriel de Jesus <devgabrieldejesus@gmail.com>
2021-02-18 08:44:19 -0300

    ✨  general commit

--- README.md
+++ README.md
@@ -1,34 +1,77 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Carousel
 
-## Getting Started
+🎠 A simple carousel with Next.js.
 
-First, run the development server:
+![](public/images/cover.gif)
 
-```bash
-npm run dev
-# or
-yarn dev
+<p align="center">
+   <img alt="Stars" src="https://img.shields.io/github/stars/gabrieldejesus/carousel?color=4A90E2&label=STARS&logo=3C424B&logoColor=3C424B&style=for-the-badge&labelColor=222222" />
+
+   <img alt="Forks" src="https://img.shields.io/github/forks/gabrieldejesus/carousel?color=4A90E2&label=FORKS&logo=3C424B&logoColor=3C424B&style=for-the-badge&labelColor=222222" />
+
+   <img alt="Issues" src="https://img.shields.io/github/issues/gabrieldejesus/carousel?color=4A90E2&label=ISSUES&logo=3C424B&logoColor=3C424B&style=for-the-badge&labelColor=222222" />
+
+   <img alt="GitHub license" src="https://img.shields.io/github/license/gabrieldejesus/carousel?color=4A90E2&label=LICENSE&logo=3C424B&logoColor=3C424B&style=for-the-badge&labelColor=222222" />
+
+  <a href="https://github.com/gabrieldejesus">
+    <img alt="Follow gabrieldejesus" src="https://img.shields.io/static/v1?label=Follow&message=gabrieldejesus&style=for-the-badge&color=4A90E2&labelColor=222222" />
+  </a>
+</p>
+
+## 🕹 Test here:
+
+_Coming soon..._
+
+## 🛠 Getting started
+
+Windows:
+
+You can clone the repository through CMD (Command Prompt) just by typing:
+
+```sh
+git clone https://github.com/gabrieldejesus/carousel
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+or download the zip from the green button at the beginning of the repository
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+<img src="https://i.ibb.co/3mLnKMH/clone.png" alt="Download zip" border="0">
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+<img src="https://i.ibb.co/3M5CXKm/clone-zip.png" alt="Download zip" border="0">
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+## 💻 Configuration for development
 
-## Learn More
+- Open project folder
 
-To learn more about Next.js, take a look at the following resources:
+- Execute command: `yarn`
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+- Execute the command: `yarn dev`
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+- In your browser enter the address: `http://localhost:3000`
 
-## Deploy on Vercel
+## 🗃 Release history
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+- 0.1.0
+  - Project finalized and studying the possibility of adding new resources
+  - The first suitable launch
+- 0.0.1
+  - Work in progress
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## 📝 Meta
+
+Gabriel de Jesus – [My portfolio](https://gabrieldesenvolvedor.com/) – devgabrieldejesus@gmail.com
+
+Distributed under the MIT License. See [LICENSE](LICENSE) for more information.
+
+[https://github.com/gabrieldejesus/carousel](https://github.com/gabrieldejesus/)
+
+## 🚀 Contribution
+
+1. Make the _fork_ of the project (<https://github.com/gabrieldejesus/carousel/fork>)
+2. Create a _branch_ for your modification (`git checkout -b my-new-resource`)
+3. Do _commit_ (`git commit -am 'Adding a new resource ...'`)
+4. _Push_ (`git push origin my-new-feature`)
+5. Create a new _Pull Request_
+
+**After your pull request is merged**, you can safely delete your branch.
+
+---
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,84 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+
+import styles from "../styles/Home.module.css";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  Dot,
+} from "pure-react-carousel";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Carousel</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,700;1,400&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <CarouselProvider
+          naturalSlideWidth={100}
+          naturalSlideHeight={125}
+          orientation="horizontal"
+          totalSlides={3}
+          className={styles.carousel}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          <Slider className={styles.carouselArea}>
+            <Slide index={0} className={styles.carouselItem}>
+              <div>
+                <img src="/images/michael-jordan.png" alt="Michael Jordan" />
+              </div>
+              <h3>“Michael Jordan”</h3>
+              <p>Eu posso aceitar falhas, mas não posso aceitar não tentar.</p>
+              <span>Jogador</span>
+            </Slide>
+            <Slide index={1} className={styles.carouselItem}>
+              <div>
+                <img src="/images/nelson-mandela.png" alt="Nelson Mandela" />
+              </div>
+              <h3>“Nelson Mandela”</h3>
+              <p>
+                Ser pela liberdade não é apenas tirar as correntes de alguém,
+                mas viver de forma que respeite e melhore a liberdade dos
+                outros.
+              </p>
+              <span>Jogador</span>
+            </Slide>
+            <Slide index={2} className={styles.carouselItem}>
+              <div>
+                <img src="/images/will-smith.png" alt="Will Smith" />
+              </div>
+              <h3>“Will Smith”</h3>
+              <p>Viva o dia como se o tivesse inventado</p>
+              <span>Ator</span>
+            </Slide>
+          </Slider>
+
+          <div className={styles.carouselButtons}>
+            <ButtonBack>
+              <RiArrowLeftSLine size={40} color="#007de7" />
+            </ButtonBack>
+            <ButtonNext>
+              <RiArrowRightSLine size={40} color="#007de7" />
+            </ButtonNext>
+          </div>
+
+          <div className={styles.carouselPoints}>
+            <Dot slide={0} />
+            <Dot slide={1} />
+            <Dot slide={2} />
+          </div>
+        </CarouselProvider>
+      </main>
     </div>
-  )
+  );
 }
